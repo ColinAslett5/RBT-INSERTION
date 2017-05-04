6975115c3a4213a7c1a903ff130e48a07748aa58
robert aslett <robert aslett>
2017-05-03 20:47:48 -0700

    Second Commit, Has Errors

--- title.cpp
+++ title.cpp
@@ -69,7 +69,112 @@ struct Node{
   }
 };
 Node* root;
-
+void add();//adding numbers manually
+bool fileadd();//adding numbers via a file
+void print();//printing out the tree
+void insert(int num);//inserting node into tree
+void preserve(Node* xx);//preserving the red black tree state
+void first(Node* node,int numb);//initial insertion is BST
 int main(){
-  cout << "HELLO!" << endl;
+  cout << "HELLO! You have entered the Red Black Tree Insertion" << endl;
+  bool kg = true;
+  while(kg == true){
+    char input;
+    cout << "you have two options, 1 = insert numbers manually, 2 = file name, 3 = quit" << endl;
+    cin >> input;
+    //adding manually
+    if(input == '1'){
+      add();
+    }
+    //adding via file
+    else if(input == '2'){
+      if(!fileadd()){
+	continue;
+      }
+    }
+    //quitting program
+    else if(input == '3'){
+      kg = false;
+      break;
+    }
+    //something that is not one of the three
+    else{
+      cout << "not one of the three inputs" << endl;
+      continue;
+    }
+    //next step
+    while(true){
+      cout << "Three options: 1 = print, 2 = add numbers manually, 3 = add numbers via file, 4 = quit" << endl;
+      cin >> input;
+      if(input == '1'){
+        cout << "RBT:" << endl;
+        print();
+      }
+      else if(input == '2'){
+        add();
+      }
+      else if(input == '3'){
+        fileadd();
+      }
+      else if(input == '4'){
+        break;
+      }
+      else{
+        cout << "Command not recognized" << endl;
+      }
+    }
+  }
+}
+//adding numbers manually
+void add(){
+  char list[200];
+  cout << "Enter numbers seperated by spaces" << endl;
+  cin.ignore();
+  cin.getline(list,200);
+  //next part
+  int index = 0;
+  while(list[index]){
+    if(isdigit(list[index])){
+      int num = list[index] - '0';
+      while(isdigit(list[++index])){
+	num = 10*num + (list[index] - '0');
+      }
+      insert(num);
+    }
+    else{
+      index++;
+    }
+  }
+}
+//adding numbers via file
+bool fileadd(){
+  
+}
+//printint out the tree
+void print(){
+  
+}
+//inserting number to node
+void insert(int num){
+  preserve(first(root,num));
+}
+//preserving the RBT properties
+void preserve(Node* xx){
+  if(xx->parent == 0){//node is head
+    xx->Color('b');
+    return;
+  }
+  else if(xx->parent->color){//parent is black
+    return;
+  }
+  else if(xx->Uncle()->color == false){//parent is red, and uncle
+    xx->parent->Color('b');
+    xx->Uncle()->Color('b');
+    xx->Grandparent()->Color('r');
+    preserve(xx->Grandparent());
+    return;
+  }
+}
+void first(Node* node,int numb){
+  
 }
